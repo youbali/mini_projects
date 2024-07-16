8ae0b645090a95d58e4e38f07d19e379177d2a37
Yassine Oubali <yassine22586@gmail.com>
2024-07-16 14:59:30 +0200

    compete the 3d project (timeRandom)

--- timeRandom.py
+++ timeRandom.py
@@ -1,9 +1,42 @@
 import random
+import time
 
-OPERATORS = ["-", "+", "7", "*"]
+#OPERATORS = ["-", "+", "/", "*"]
+OPERATORS = ["+", "/"]
 MIN_OPERAND = 3
 MAX_OPERAND = 12
+OPERATORS.remove(OPERATORS[1])
+TOTAL_PROBLEMS = 10
 
 def genarate_problem():
     left = random.randint(MIN_OPERAND, MAX_OPERAND)
     right = random.randint(MIN_OPERAND, MAX_OPERAND)
+    operator = random.choice(OPERATORS)
+
+    expr = str(left)+" "+ operator +" "+str(right)
+    result = eval(expr)
+    #print(expr," = ", result)
+    return expr, result
+
+#genarate_problem()
+
+wrong = 0
+input("Press Enter to start")
+print("----------------------")
+
+start_time = time.time()
+
+for i in range (TOTAL_PROBLEMS):
+    expr, result = genarate_problem()
+    while True:
+        guess = input("PROBLEM "+str(i+1)+": "+expr+" = ")
+        if guess == str(result):
+            break
+        wrong += 1
+        print("fals! Try again:")
+
+end_time = time.time()
+total_time = round(end_time - start_time, 2)
+print("----------------------")
+print("du hast " +str(wrong)+ " Fehler gemacht")
+print("Good work, you finished in ", total_time, " seconds")
